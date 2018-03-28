524c8b35ac6b4a10dcd731bf3a59982faf6f9138
tbhattacharya_HA <tbhattacharya@hostanalytics.com>
2018-03-28 16:52:50 +0530

    Added Heroku-Postbuild

--- index.js
+++ index.js
@@ -1,37 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const keys=require('./config/keys');
-const passport=require('passport');
-const cookiesSession = require('cookie-session');
+const express = require("express");
+const mongoose = require("mongoose");
+const keys = require("./config/keys");
+const passport = require("passport");
+const cookiesSession = require("cookie-session");
 require("./models/User");
 require("./services/passport");
 
-
 mongoose.connect(keys.mongoconnectionstring);
 
-const app =express();
+const app = express();
 
-app.use(cookiesSession({
-    maxAge:30*24*60*60*1000,
-    keys :[keys.cookieKey]
-}))
+app.use(
+  cookiesSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 
 app.use(passport.initialize());
 app.use(passport.session());
 
 require("./routes/authRoutes")(app);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
-
-
-/*
-app.get('/',(req,res)=>{
-    res.send({Name:'Project'})
-})
-*/
-
-
-
-
-const PORT =process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
