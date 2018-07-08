364392efa608b67a1d5de16a88215f64c3babb33
tbhattacharya_HA <tbhattacharya@hostanalytics.com>
2018-07-09 01:36:10 +0530

    commit

--- config/prod.js
+++ config/prod.js
@@ -2,6 +2,9 @@ module.exports = {
   googleClientID: process.env.googleClientID,
   googleClientSecret: process.env.googleClientSecret,
   mongoconnectionstring: process.env.mongoconnectionstring,
-  cookieKey: process.env.cookieKey,
-  sendGridKey: process.env.sendGridKey
+  cookieKey: "asdfghjklqwsdfvboijhbbv",
+  sendGridKey:
+    "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",
+  elasticHost: "http://DELLPREC-2218.haicpm.com:9201",
+  elasticAuth: "logstash:logstash"
 };
